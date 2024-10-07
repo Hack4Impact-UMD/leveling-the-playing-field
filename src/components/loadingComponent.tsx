48bd9bc4e025355078fe9f47e489ba4e54365831
Varun Rao <varunvellorerao@gmail.com>
2024-10-06 23:49:24 -0400

    added loading component

---
+++ src/components/loadingComponent.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const LoadingComponent = () => {
+  return (
+    <div className="flex flex-col items-center justify-center w-screen h-screen bg-white">
+      <div className="flex p-4">
+        <div className="bg-orange rounded-full h-9 w-9 m-1"></div>
+        <div className="bg-orange-400 rounded-full h-9 w-9 m-1"></div>
+        <div className="bg-orange-light rounded-full h-9 w-9 m-1"></div>
+      </div>
+      <h1 className="text-black text-3xl" style={{ fontFamily: 'var(--font-geist-sans)', fontWeight: 'bold' }}>Loading page, please wait...</h1>
+    </div>
+  )
+}
+
+export default LoadingComponent;
