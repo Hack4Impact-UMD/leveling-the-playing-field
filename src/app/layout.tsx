29f886a6fec704a3df0ce52de36ef6f4403cb755
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2024-10-14 01:35:22 -0400

    added fonts from the Figma to the project using next/font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,28 +2,40 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 const breeSerif = localFont({
-  src: "./fonts/BreeSerif-Regular.ttf",
-  variable: "--font-bree-serif",
-  weight: "400", 
+  src: "@/../public/fonts/BreeSerif-Regular.ttf",
+  weight: "400",
   style: "normal",
+  variable: "--font-bree-serif",
+});
+const cabin = localFont({
+  src: "@/../public/fonts/Cabin-Regular.ttf",
+  weight: "400",
+  style: "normal",
+  variable: "--font-cabin",
 });
 const cabinCondensed = localFont({
-  src: "./fonts/CabinCondensed-Regular.ttf",
+  src: [
+    {
+      path: "@/../public/fonts/CabinCondensed-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "@/../public/fonts/CabinCondensed-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    }
+  ],
   variable: "--font-cabin-condensed",
-  weight: "400", 
-  style: "normal",
 });
+const ubuntuCondensed = localFont({
+  src: "@/../public/fonts/UbuntuCondensed-Regular.ttf",
+  weight: "400",
+  style: "normal",
+  variable: "--font-ubuntu-condensed",
+});
+
 export const metadata: Metadata = {
   title: "Leveling the Playing Field",
   description: "Equipment checkout application for organizations working with the non-profit Leveling the Playing Field",
@@ -37,7 +49,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${breeSerif.variable} ${cabinCondensed.variable} antialiased`}
+        className={`${breeSerif.variable} ${cabin.variable} ${cabinCondensed.variable} ${ubuntuCondensed.variable} antialiased`}
       >
         {children}
       </body>
