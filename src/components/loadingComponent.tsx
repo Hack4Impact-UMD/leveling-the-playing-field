113bb995de0e3ddce313a1266e0b2e217cac8f95
lanceu0128 <lanceu0128@gmail.com>
2024-10-15 17:16:21 -0400

    use tailwind for fonts in loading component

--- src/components/loadingComponent.tsx
+++ src/components/loadingComponent.tsx
@@ -9,8 +9,8 @@ const LoadingComponent = () => {
         <div className="animate-[bounce_1s_ease-in-out_700ms_infinite] bg-orange-light rounded-full h-9 w-9 m-1"></div>
       </div>
       <div className="text-black text-2xl text-center">
-        <h1 className="text-black text-3xl text-center" style={{ fontFamily: 'var(--font-bree-serif)' }}>Loading</h1>
-        <h1 className="text-black text-3xl text-center" style={{ fontFamily: 'var(--font-bree-serif)' }}>Please wait...</h1>
+        <h1 className="text-black text-3xl text-center font-bree-serif">Loading</h1>
+        <h1 className="text-black text-3xl text-center font-bree-serif" >Please wait...</h1>
       </div>
     </div>
   )
