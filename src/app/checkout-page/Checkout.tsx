4078538ddf1404dd0ca6d0b428877b91743731ed
Viraj <viraj.viraj0002@gmail.com>
2024-10-19 18:50:44 -0400

    added checkout icon. moved sport select outside SelectEquipment Component

--- src/app/checkout-page/Checkout.tsx
+++ src/app/checkout-page/Checkout.tsx
@@ -1,11 +1,14 @@
 "use client";
 import React, { useState } from "react";
 import SelectEquipment from "./SelectEquipment";
+import ShoppingCartIcon from "@/components/icons/ShoppingCartIcon";
 
 const Checkout = () => {
   const [equipmentList, setEquipmentList] = useState([
     { id: 1, name: "Select Equipment", quantity: 1 },
   ]);
+
+  const [sport, setSport] = useState<string>("")
 
   const addEquipment = () => {
     const last = equipmentList.at(equipmentList.length - 1)?.id ?? 0;
@@ -19,14 +22,30 @@ const Checkout = () => {
     setEquipmentList(equipmentList.filter((item) => item.id !== id));
   };
 
+  const handleSport = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = event.target.value;
+    setSport(String(value));
+  }
+
   return (
     <div className="flex flex-col items-center bg-white h-screen p-8 overflow-scroll">
-      <div className="mb-6">
-        <img src="/_images/logo.png" alt="LPF Logo" className="w-20 h-auto" />
+      <h1 className="text-3xl font-bold mb-4 text-black">Add Equipment</h1>
+      <div className="mb-6 rounded-full w-24 h-24 bg-teal text-white flex items-center justify-center p-4">
+        <ShoppingCartIcon />
       </div>
-      <h1 className="text-3xl font-bold mb-8 text-black">Checkout</h1>
 
       <div className="w-full max-w-md space-y-4">
+        <select
+          value={sport}
+          onChange={handleSport}
+          className="w-full bg-teal text-white py-2 px-4 rounded-md font-semibold text-center"
+        >
+          <option value="" disabled>Select Sport</option>
+          <option value="soccer">Soccer</option>
+          <option value="basketball">Basketball</option>
+          <option value="baseball">Baseball</option>
+          <option value="tennis">Tennis</option>
+        </select>
         {equipmentList.map((item) => (
           <SelectEquipment
             key={item.id}
@@ -34,12 +53,12 @@ const Checkout = () => {
           />
         ))}
 
-        <button
+        {/* <button
           onClick={addEquipment}
           className="w-full bg-teal text-white py-2 px-4 rounded-md font-semibold"
         >
           + Add Equipment
-        </button>
+        </button> */}
       </div>
 
       <div className="w-full max-w-md mt-auto">
