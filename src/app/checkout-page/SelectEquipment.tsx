53e216b1112dbb95dcfbcaba6377a61014a5a950
Paradox560 <65742681+Paradox560@users.noreply.github.com>
2024-10-28 16:40:03 -0400

    added sport removal capabilities

--- src/app/checkout-page/SelectEquipment.tsx
+++ src/app/checkout-page/SelectEquipment.tsx
@@ -22,10 +22,10 @@ const sportsIconsMap = new Map<string, JSX.Element>([
 ]);
 
 const sportsItemsMap = new Map<string, string[]>([
-    ['soccer', ['Soccer Cleats']],
-    ['baseball', ['Baseball Mitts']],
-    ['tennis', ['Tennis Rackets']],
-    ['basketball', ['Basketball']]
+    ['soccer', ['Soccer Cleats', 'Soccer Ball']],
+    ['baseball', ['Baseball Mitts', 'Baseball Bat']],
+    ['tennis', ['Tennis Rackets', 'Tennis Ball']],
+    ['basketball', ['Basketball', 'Basketball Shoes']]
 ]);
 
 const SelectEquipment = ({ removeSport, removeEquipment, selectSport, updateEquipment, sport, equipmentList }: Props) => {
@@ -39,6 +39,7 @@ const SelectEquipment = ({ removeSport, removeEquipment, selectSport, updateEqui
         updatedEquipmentList[index].quantity = value;
         updateEquipment(updatedEquipmentList);
     };
+
     const handleSportSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
         if (e.target.value === "") {
             return;
@@ -74,8 +75,29 @@ const SelectEquipment = ({ removeSport, removeEquipment, selectSport, updateEqui
                 </select>
             ) : (
                 <>
-                    <div className="flex-1 bg-teal text-white text-center py-2.5 rounded-md font-semibold w-full sm:w-auto">
-                        {sport}
+                    <div className="flex justify-between space-x-2 items-center w-full">
+                        <div className="flex-row items-center bg-teal text-white py-2 px-2 rounded-md font-semibold">
+                            {sport}
+                        </div>
+                        <button
+                            className="text-red-600 flex-none font-bold py-2 rounded-md text-lg sm:w-auto"
+                            onClick={() => removeSport(sport)}
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth="1.5"
+                                stroke="currentColor"
+                                className="size-6"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1-3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                                />
+                            </svg>
+                        </button>
                     </div>
                     {equipmentList.map((equipment, index) => (
                         <div key={index} className="flex justify-between space-x-2 items-center w-full">
