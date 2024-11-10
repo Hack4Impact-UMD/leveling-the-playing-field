c0f80431e1333ebc0b9b6174d16ca7a90c926af8
Sara Karnik <skarnik1@terpmail.umd.edu>
2024-11-10 12:28:44 -0500

    1

---
+++ src/app/thankyou/thankyouPage.tsx
@@ -0,0 +1,11 @@
+// app/thankyou/thankyoupage.tsx
+import React from 'react';
+
+export default function ThankYouPage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-200">
+      <h1 className="text-6xl font-bold text-black mb-4">Thank you!</h1>
+      <p className="text-xl text-teal-800">Your order was completed successfully!</p>
+    </div>
+  );
+}
