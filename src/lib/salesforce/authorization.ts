aa219d83d524c62435252e27f46bb14279624c0d
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2024-11-17 15:34:17 -0500

    made necessary .env variables available to the client

--- src/lib/salesforce/authorization.ts
+++ src/lib/salesforce/authorization.ts
@@ -8,13 +8,13 @@ export function getSalesforceAuthURL(): string {
 }
 
 export async function getTokensFromAuthCode(authCode: string) {
-  const url = new URL("/services/oauth2/token", process.env.NEXT_PUBLIC_SALESFORCE_DOMAIN)
+  const url = new URL("/services/oauth2/token", process.env.SALESFORCE_DOMAIN)
   const searchParams = url.searchParams;
   searchParams.set("code", authCode);
   searchParams.set("grant_type", "authorization_code");
-  searchParams.set("client_id", process.env.NEXT_PUBLIC_SALESFORCE_CLIENT_ID || "");
-  searchParams.set("client_secret", process.env.NEXT_PUBLIC_SALESFORCE_CLIENT_SECRET || "");
-  searchParams.set("redirect_uri", process.env.NEXT_PUBLIC_SALESFORCE_REDIRECT_URI || "");
+  searchParams.set("client_id", process.env.SALESFORCE_CLIENT_ID || "");
+  searchParams.set("client_secret", process.env.SALESFORCE_CLIENT_SECRET || "");
+  searchParams.set("redirect_uri", process.env.SALESFORCE_REDIRECT_URI || "");
   const res = await fetch(url.toString(), { method: "POST" });
   const body = await res.json();
   return {
@@ -24,11 +24,11 @@ export async function getTokensFromAuthCode(authCode: string) {
 }
 
 export async function refreshAccessToken(refreshToken: string) {
-  const url = new URL("/services/oauth2/token", process.env.NEXT_PUBLIC_SALESFORCE_DOMAIN);
+  const url = new URL("/services/oauth2/token", process.env.SALESFORCE_DOMAIN);
   const searchParams = url.searchParams;
   searchParams.set("grant_type", "refresh_token");
-  searchParams.set("client_id", process.env.NEXT_PUBLIC_SALESFORCE_CLIENT_ID || "");
-  searchParams.set("client_secret", process.env.NEXT_PUBLIC_SALESFORCE_CLIENT_SECRET || "");
+  searchParams.set("client_id", process.env.SALESFORCE_CLIENT_ID || "");
+  searchParams.set("client_secret", process.env.SALESFORCE_CLIENT_SECRET || "");
   searchParams.set("refresh_token", refreshToken);
   const res = await fetch(url.toString(), { method: "POST" });
   const body = await res.json();
