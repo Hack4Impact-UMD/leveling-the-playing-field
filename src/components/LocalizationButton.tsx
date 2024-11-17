c24f46ec97f2cc0d190cb8b01b2e7499a32c4c87
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2024-11-17 14:06:00 -0500

    moved and renamed icons

--- src/components/LocalizationButton.tsx
+++ src/components/LocalizationButton.tsx
@@ -2,9 +2,9 @@
 import React, { useState } from "react";
 import Globe from "./icons/Globe";
 import Chevron from "./icons/Chevron";
-import USFlag from "./icons/Flag_of_the_United_States";
-import FrenchFlag from "./icons/Flag_of_France";
-import SpanishFlag from "./icons/Flag_of_Spain";
+import USFlagIcon from "./icons/flags/USFlagIcon";
+import FrenchFlagIcon from "./icons/flags/FrenchFlagIcon";
+import SpanishFlagIcon from "./icons/flags/SpanishFlagIcon";
 
 export default function LocalizationButton() {
     const [isOpen, setIsOpen] = useState(false);
@@ -27,21 +27,21 @@ export default function LocalizationButton() {
                         onClick={() => selectLanguage("English")}
                         className="flex items-center px-2 py-2 text-gray-800 hover:bg-gray-200 cursor-pointer space-x-2"
                     >
-                        <USFlag width={35} height={25} />
+                        <USFlagIcon width={35} height={25} />
                         <span>English</span>
                     </div>
                     <div
                         onClick={() => selectLanguage("Español")}
                         className="flex items-center px-2 py-2 text-gray-800 hover:bg-gray-200 cursor-pointer"
                     >
-                        <SpanishFlag width={45} height={35} />
+                        <SpanishFlagIcon width={45} height={35} />
                         <span>Español</span>
                     </div>
                     <div
                         onClick={() => selectLanguage("Français")}
                         className="flex items-center px-2 py-2 text-gray-800 hover:bg-gray-200 cursor-pointer space-x-2"
                     >
-                        <FrenchFlag width={35} height={22} />
+                        <FrenchFlagIcon width={35} height={22} />
                         <span>Français</span>
                     </div>
                 </div>
