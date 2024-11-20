96849a17a1e9aa260ebb47b703fcd06d5f51cf76
Varun Rao <varunvellorerao@gmail.com>
2024-11-20 14:24:51 -0500

    main framework in temp file for now

--- src/app/api/salesforce/route.ts
+++ src/app/api/salesforce/route.ts
@@ -8,3 +8,37 @@ export async function GET(req: NextRequest) {
   );
   return NextResponse.json(tokens, { status: 200 });
 }
+
+interface Contact {
+  id: string;
+  name: string;
+  email: string;
+  phone: string;
+}
+
+interface ErrorResponse {
+  message: string;
+  error?: string;
+}
+
+interface SuccessResponse {
+  contacts: Contact[];
+}
+
+export async function contactGET(req: NextRequest, { params }: { params: { id: string } }) {
+  // requires accountId query parameter - get list of contacts associated with the accountId
+}
+
+export async function opportunityGET(req: NextRequest, { params }: { params: { id: string } }) {
+  // requires accountId and status query parameters - get list of opportunities associated with the accountId with the given status
+}
+
+export async function productGET(req: NextRequest, { params }: { params: { id : string }}) {
+  // get the full list of products from the Salesforce Price Book
+}
+
+export async function opportunityPOST(req: NextRequest) {
+  // loop through all the events of each warehouse’s Calendly calendar and create new opportunities in 
+  // Salesforce for new event sign-ups. Delete opportunities if someone has cancelled a signup, update if they’ve rescheduled
+}
+
