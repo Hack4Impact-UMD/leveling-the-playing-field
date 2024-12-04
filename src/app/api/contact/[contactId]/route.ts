ca0c2120cc00589218065cc452f5a3adf5bb5610
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2024-12-03 19:29:25 -0500

    moved all functions handling Contact objects to their own file

--- src/app/api/contact/[contactId]/route.ts
+++ src/app/api/contact/[contactId]/route.ts
@@ -1,119 +1,36 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { refreshAccessToken } from '@/lib/salesforce/authorization';
-import { Contact } from '../../../../types';
+import { deleteContact, getContactById, updateContact } from '@/lib/salesforce/database/contact';
+import { isError } from '@/types/apiTypes';
 
-const NEXT_PUBLIC_SALESFORCE_CLIENT_ID = String(process.env.NEXT_PUBLIC_SALESFORCE_CLIENT_ID);
-const NEXT_PUBLIC_SALESFORCE_CLIENT_SECRET = String(process.env.NEXT_PUBLIC_SALESFORCE_CLIENT_SECRET);
-const SALESFORCE_REFRESH_TOKEN = String(process.env.SALESFORCE_REFRESH_TOKEN)
-
-async function getSalesforceToken() {
-    const response = await fetch('https://login.salesforce.com/services/oauth2/token', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        body: new URLSearchParams({
-            grant_type: 'refresh_token',
-            client_id: NEXT_PUBLIC_SALESFORCE_CLIENT_ID,
-            client_secret: NEXT_PUBLIC_SALESFORCE_CLIENT_SECRET,
-            refresh_token: SALESFORCE_REFRESH_TOKEN,
-        })
-    });
-
-    const data = await response.json();
-    return data.access_token;
-}
-
-async function querySalesforce(query: string) {
-    const access_token = await getSalesforceToken();
-
-    const response = await fetch(`https://connect-customization-2394.my.salesforce.com/services/data/v62.0/query?q=${encodeURIComponent(query)}`, {
-        headers: {
-            Authorization: `Bearer ${access_token}`
-        }
-    });
-
-    const data = await response.json();
-    console.log(data.records); // Logs retrieved records
-    return data
-}
-
-
-
-export async function GET(request: Request, { params }: { params: { contactId : string } }) {
+export async function GET(req: NextRequest, { params }: { params: { contactId : string } }) {
   try {
     const accessToken = await refreshAccessToken(process.env.SALESFORCE_REFRESH_TOKEN || "");
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_SALESFORCE_DOMAIN}/services/data/v56.0/sobjects/Contact/${params.contactId}`,
-      {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      }
-    );
-
-    if (!response.ok) {
-      const error = await response.json();
-      return NextResponse.json(error, { status: response.status });
-    }
-
-    const data = await response.json();
-    return NextResponse.json(data, { status: 200 });
+    const res = await getContactById(params.contactId, accessToken);
+    return NextResponse.json(isError(res) ? res.error : res.data, { status: res.status });
   } catch (error) {
     console.error('Salesforce API Error:', error);
     return NextResponse.json({ error: 'Error processing request' }, { status: 500 });
   }
 }
 
-// PUT
-export async function PUT(request: Request, { params }: { params: { contactId: string } }) {
+export async function PUT(req: NextRequest, { params }: { params: { contactId: string } }) {
   try {
     const accessToken = await refreshAccessToken(process.env.SALESFORCE_REFRESH_TOKEN || "");
-    const body = await request.json();
-
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_SALESFORCE_DOMAIN}/services/data/v56.0/sobjects/Contact/${params.contactId}`,
-      {
-        method: "PATCH",
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(body),
-      }
-    );
-
-    if (!response.ok) {
-      const error = await response.json();
-      return NextResponse.json(error, { status: response.status });
-    }
-
-    return NextResponse.json({ success: true }, { status: 200 });
+    const body = await req.json();
+    const response = await updateContact(params.contactId, accessToken, body);
+    return NextResponse.json(isError(response) ? response.error : response.data, { status: response.status === 204 ? 200 : response.status });
   } catch (error) {
     console.error('Salesforce API Error:', error);
     return NextResponse.json({ error: 'Error processing request' }, { status: 500 });
   }
 }
 
-// DELETE
-export async function DELETE(request: Request, { params }: { params: { contactId: string } }) {
+export async function DELETE(req: NextRequest, { params }: { params: { contactId: string } }) {
   try {
     const accessToken = await refreshAccessToken(process.env.SALESFORCE_REFRESH_TOKEN || "");
-    
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_SALESFORCE_DOMAIN}/services/data/v56.0/sobjects/Contact/${params.contactId}`,
-      {
-        method: "DELETE",
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      }
-    );
-
-    if (!response.ok) {
-      const error = await response.json();
-      return NextResponse.json(error, { status: response.status });
-    }
-
-    return NextResponse.json({ success: true }, { status: 200 });
+    const response = await deleteContact(params.contactId, accessToken);
+    return NextResponse.json(isError(response) ? response.error : response.data, { status: response.status === 204 ? 200 : response.status });
   } catch (error) {
     console.error('Salesforce API Error:', error);
     return NextResponse.json({ error: 'Error processing request' }, { status: 500 });
--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,32 +1,17 @@
 import { NextResponse } from 'next/server';
 import { refreshAccessToken } from '@/lib/salesforce/authorization';
+import { Contact } from '@/types/types';
+import { createContact } from '@/lib/salesforce/database/contact';
+import { isError } from '@/types/apiTypes';
 
 export async function POST(request: Request) {
   try {
     const accessToken = await refreshAccessToken(process.env.SALESFORCE_REFRESH_TOKEN || "");
-    const body = await request.json();
-
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_SALESFORCE_DOMAIN}/services/data/v56.0/sobjects/Contact`,
-      {
-        method: 'POST',
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(body),
-      }
-    );
-
-    if (!response.ok) {
-      const error = await response.json();
-      return NextResponse.json(error, { status: response.status });
-    }
-
-    const data = await response.json();
-    return NextResponse.json(data, { status: 201 });
+    const body: Contact = await request.json();
+    const response = await createContact(accessToken, body);
+    return NextResponse.json(isError(response) ? response.error : response.data, { status: response.status });
   } catch (error) {
     console.error('Salesforce API Error:', error);
-    return NextResponse.json({ error: 'Error creating opportunity' }, { status: 500 });
+    return NextResponse.json({ error: 'Error creating contact' }, { status: 500 });
   }
 } 
