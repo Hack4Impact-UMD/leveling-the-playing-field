ca0d703ef653a9665f9da812f4ca0f153e0611bb
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2024-12-23 22:47:02 -0500

    moved all functions accessing Salesforce to separate data access from API endpoints

--- src/app/api/opportunity/[opportunityId]/route.ts
+++ src/app/api/opportunity/[opportunityId]/route.ts
@@ -1,176 +1,93 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { refreshAccessToken } from '@/lib/salesforce/authorization';
 import { executeSOQLQuery } from '@/lib/salesforce/soqlQuery';
-
-interface QueryResponse<T> {
-  totalSize: number;
-  done: boolean;
-  records: Array<T>;
-}
-
-interface OpportunityProduct {
-  pricebookEntryId: string;
-  quantity: number;
-}
+import { APIResponse, isError } from '@/types/apiTypes';
+import { createOpportunityLineItem, deleteOpportunity, getOpportunityById, updateOpportunity, updateOpportunityLineItem } from '@/lib/salesforce/database/opportunity';
+import { CheckoutItem, Opportunity, OpportunityLineItem, PricebookEntry } from '@/types/types';
 
 export async function GET(
-  request: Request,
+  request: NextRequest,
   { params }: { params: { opportunityId: string } }
 ) {
   try {
     const accessToken = await refreshAccessToken(process.env.SALESFORCE_REFRESH_TOKEN || "");
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_SALESFORCE_DOMAIN}/services/data/v56.0/sobjects/Opportunity/${params.opportunityId}`,
-      {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      }
-    );
-
-    if (!response.ok) {
-      const error = await response.json();
-      return NextResponse.json(error, { status: response.status });
-    }
-
-    const data = await response.json();
-    return NextResponse.json(data, { status: 200 });
+    const res = await getOpportunityById(params.opportunityId, accessToken);
+    return NextResponse.json(isError(res) ? res.error : res.data, { status: res.status });
   } catch (error) {
     console.error('Salesforce API Error:', error);
     return NextResponse.json({ error: 'Error processing request' }, { status: 500 });
-  }
-}
-
-async function updateOpportunityProducts(opportunityId: string, products: OpportunityProduct[]) {
-  const accessToken = await refreshAccessToken(process.env.SALESFORCE_REFRESH_TOKEN || "");
-  const salesforceDomain = process.env.NEXT_PUBLIC_SALESFORCE_DOMAIN;
-
-  try {
-    for (const product of products) {
-      const { pricebookEntryId, quantity } = product;
-
-      const unitPrice: QueryResponse<{ UnitPrice: number }> = await executeSOQLQuery(`SELECT UnitPrice FROM PricebookEntry WHERE Id='${pricebookEntryId}'`);
-      if (unitPrice.totalSize == 0) {
-        throw new Error(`Error: No UnitPrice found for pricebookEntryId ${pricebookEntryId}`)
-      }
-
-      const existingLineItemData: QueryResponse<{ Id: string }> = await executeSOQLQuery(`SELECT Id FROM OpportunityLineItem WHERE OpportunityId='${opportunityId}' AND PricebookEntryId='${pricebookEntryId}'`);
-
-      if (existingLineItemData.records.length > 0) {
-        const existingLineItemId = existingLineItemData.records[0].Id;
-
-        const updateResponse = await fetch(
-          `${salesforceDomain}/services/data/v56.0/sobjects/OpportunityLineItem/${existingLineItemId}`,
-          {
-            method: 'PATCH',
-            headers: {
-              Authorization: `Bearer ${accessToken}`,
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-              Quantity: quantity,
-            }),
-          }
-        );
-
-        if (!updateResponse.ok) {
-          const error = await updateResponse.json();
-          throw new Error('Error updating OpportunityLineItem: ', error);
-        }
-      } else {
-        const createResponse = await fetch(
-          `${salesforceDomain}/services/data/v56.0/sobjects/OpportunityLineItem`,
-          {
-            method: 'POST',
-            headers: {
-              Authorization: `Bearer ${accessToken}`,
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-              OpportunityId: opportunityId,
-              PricebookEntryId: pricebookEntryId,
-              Quantity: quantity,
-              UnitPrice: unitPrice.records[0].UnitPrice
-            }),
-          }
-        );
-
-        if (!createResponse.ok) {
-          const error = await createResponse.json();
-          throw new Error('Error creating OpportunityLineItem:', error);
-        }
-      }
-    }
-  } catch (error) {
-    console.error('Error updating Opportunity products:', error);
-    throw error;
   }
 }
 
 export async function PUT(
-  request: Request,
+  request: NextRequest,
   { params }: { params: { opportunityId: string } }
 ) {
   try {
+    const { opportunityId } = params;
     const accessToken = await refreshAccessToken(process.env.SALESFORCE_REFRESH_TOKEN || "");
+    
+    const postedCheckRes: APIResponse<Opportunity> = await getOpportunityById(opportunityId, accessToken);
+    if (isError(postedCheckRes)) {
+      return NextResponse.json({ error: "Error processing request" }, { status: 500 });
+    } else if (postedCheckRes.data.StageName === "Posted") {
+      return NextResponse.json({ message: "Opportunity is already posted" }, { status: 403 });
+    }
+    
     const body = await request.json();
-    if (body.products) {
-      await updateOpportunityProducts(params.opportunityId, body.products);
+    if (!body.products || body.products.length === 0) {
+      return NextResponse.json({ error: "Bad Request" }, { status: 400 });
     }
 
-    const filteredBody = Object.fromEntries(
-      Object.entries(body).filter(([key]) => key !== 'products')
-    );
-
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_SALESFORCE_DOMAIN}/services/data/v56.0/sobjects/Opportunity/${params.opportunityId}`,
-      {
-        method: "PATCH",
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(filteredBody),
+    const responses = await Promise.all(body.products.map(async (product: CheckoutItem): Promise<APIResponse<{ success: true }>> => {
+      const existenceCheckRes: APIResponse<OpportunityLineItem[]> = await executeSOQLQuery(`SELECT Id FROM OpportunityLineItem WHERE OpportunityId = '${opportunityId}' AND PricebookEntryId = '${product.PricebookEntryId}'`);
+      if (isError(existenceCheckRes)) {
+        return existenceCheckRes;
       }
-    );
+      const exists = existenceCheckRes.data.length > 0;
+      if (exists) {
+        return updateOpportunityLineItem(existenceCheckRes.data[0].Id || "", accessToken, { Quantity: product.Quantity });
+      }
 
-    if (!response.ok) {
-      const error = await response.json();
-      return NextResponse.json(error, { status: response.status });
-    }
+      const unitPriceRes: APIResponse<{ UnitPrice: number }[]> = await executeSOQLQuery(`SELECT UnitPrice FROM PricebookEntry WHERE Id = '${product.PricebookEntryId}'`)
+      return isError(unitPriceRes) ? unitPriceRes :
+        createOpportunityLineItem({
+          OpportunityId: opportunityId,
+          ...product,
+          UnitPrice: unitPriceRes.data[0].UnitPrice
+        }, accessToken)
+    }))
 
-    return NextResponse.json({ success: true }, { status: 200 });
+    responses.forEach((res: APIResponse<{ success: true }>) => {
+      if (isError(res)) {
+        return NextResponse.json({ message: "Unable to checkout all items"}, { status: 500 })
+      }
+    });
+
+    const res = await updateOpportunity(opportunityId, accessToken, { StageName: "Posted" });
+    return NextResponse.json(isError(res) ? res.error : { success: true }, { status: res.status === 204 ? 200 : res.status });
   } catch (error) {
-    console.error('Salesforce API Error:', error);
-    return NextResponse.json({ error: 'Error processing request' }, { status: 500 });
+    console.error("Salesforce API Error:", error);
+    return NextResponse.json(
+      { error: "Error processing request" },
+      { status: 500 }
+    );
   }
 }
 
 export async function DELETE(
-  request: Request,
+  request: NextRequest,
   { params }: { params: { opportunityId: string } }
 ) {
   try {
     const accessToken = await refreshAccessToken(process.env.SALESFORCE_REFRESH_TOKEN || "");
-
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_SALESFORCE_DOMAIN}/services/data/v56.0/sobjects/Opportunity/${params.opportunityId}`,
-      {
-        method: "DELETE",
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      }
-    );
-
-    if (!response.ok) {
-      const error = await response.json();
-      return NextResponse.json(error, { status: response.status });
-    }
-
-    return NextResponse.json({ success: true }, { status: 200 });
+    const res = await deleteOpportunity(params.opportunityId, accessToken);
+    return NextResponse.json(isError(res) ? res.error : res.data, { status: res.status });
   } catch (error) {
-    console.error('Salesforce API Error:', error);
-    return NextResponse.json({ error: 'Error processing request' }, { status: 500 });
+    console.error("Salesforce API Error:", error);
+    return NextResponse.json(
+      { error: "Error processing request" },
+      { status: 500 }
+    );
   }
 } 
