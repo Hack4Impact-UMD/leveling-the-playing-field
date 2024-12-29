cb31f002b2080839d793a83d64f5ca97390c60cd
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2024-12-28 19:17:40 -0500

    updated fonts to match Figma

--- src/app/[lang]/thankyou/thankyouPage.tsx
+++ src/app/[lang]/thankyou/thankyouPage.tsx
@@ -31,14 +31,14 @@ export default function ThankYouPage(props: ThankYouPageProps) {
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-200">
       <Image src={LPFLogo} width={100} height={100} alt="LPF logo" />
       <br />
-      <h1 className="text-6xl font-bold text-black mb-4">
+      <h1 className="text-6xl font-bold text-black mb-4 font-bree-serif">
         {dict?.thankYouPage.thankYou.text}
       </h1>
-      <p className="text-xl text-teal-800 mb-6">
+      <p className="text-xl text-teal-800 mb-6 font-cabin-condensed">
         {dict?.thankYouPage.successfulCheckout.text}
       </p>
       <a href="/receipt">
-        <button className="bg-teal-600 text-white font-medium py-2 px-4 rounded-lg hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2 transition-all duration-200">
+        <button className="bg-teal-600 text-white font-medium py-2 px-4 rounded-lg hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2 transition-all duration-200 font-cabin-condensed">
           {dict?.thankYouPage.viewReceipt.text}
         </button>
       </a>
